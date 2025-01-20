a0dbcc6217537c2490420d33e428a092f41ae3d8
Gaurav-Gilli <singhgaurav155.gs@gmail.com>
2025-01-20 12:56:14 +0530

    security

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,24 +1,44 @@
 module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::errors",
+  /* Beginning of snippet */
   {
-    name: 'strapi::security',
+    name: "strapi::security",
     config: {
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          'connect-src': ["'self'", 'http:', 'https:', "https://youngstar-backend.onrender.com"],
-          "script-src":["'self'", 'http:', 'https:', "https://youngstar-backend.onrender.com"],
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            "https://youngstar-backend.onrender.com",
+            "youngstar-backend.onrender.com",
+            "s3.ap-south-1.amazonaws.com",
+            "market-assets.strapi.io",
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            "skoliko.s3.ap-south-1.amazonaws.com",
+            "s3.ap-south-1.amazonaws.com",
+            "market-assets.strapi.io",
+          ],
           upgradeInsecureRequests: null,
-        }
+        },
       },
     },
   },
+  /* End of snippet */
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
